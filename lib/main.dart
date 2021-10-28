36c726b23c263042ea24ed579952dec9c06404a5
rachmanmarjianto <rachmanmarjianto@gmail.com>
2021-10-28 07:27:19 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'pages/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(
+        seconds: 8,
+        navigateAfterSeconds: Login(),
+        title: new Text(
+          'Belajar Flutter',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.white
+          ),
+        ),
+        image: Image.asset('assets/unair_mbkm.png'),
+        photoSize: 100,
+        backgroundColor: Colors.blue,
+        loaderColor: Colors.white,
+      ),
+    );
+  }
+}
---
+++ lib/pages/login.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/logo.png',
+                height: 200,
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 70),
+                child: Text(
+                  "New Experience",
+                  style: new TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top:20),
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: Text(
+                  "Watch a new movie much easier than any before",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 20
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 80),
+                child: ElevatedButton(
+                    onPressed: (){},
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "Get Started",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.purple
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(5),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Already have an account? "),
+                  GestureDetector(
+                    onTap: (){
+                      print("hallo");
+                    },
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(
+                          color: Colors.purple
+                      ),
+                    ),
+                  )
+
+                ],
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }
+}
