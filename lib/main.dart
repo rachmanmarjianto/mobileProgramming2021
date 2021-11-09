9f1c067638f9b0eca0abaa179e3d39bbd8a399b9
mochoktas <tian.saktiawan@gmail.com>
2021-11-09 22:58:57 +0700

    commit uts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
-import 'pages/login.dart';
+import 'pages/halaman_1.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,26 +12,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SplashScreen(
-        seconds: 8,
-        navigateAfterSeconds: Login(),
-        title: new Text(
-          'Belajar Flutter',
-          style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              color: Colors.white
-          ),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
         ),
-        image: Image.asset('assets/unair_mbkm.png'),
-        photoSize: 100,
-        backgroundColor: Colors.blue,
-        loaderColor: Colors.white,
-      ),
-    );
+        home: SplashScreen(
+          seconds: 10,
+          navigateAfterSeconds: firstPage(),
+          title: new Text(
+            'Welcome',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 25, color: Colors.white),
+          ),
+          image: Image.asset('assets/Tango_icon.png'),
+          photoSize: 125,
+          backgroundColor: Colors.black,
+          loaderColor: Colors.white,
+        ));
   }
 }
