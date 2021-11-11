01bd271c6f1de1f3afa01869be0dc89cf3b59f84
sulthanbt <92357171+sulthanbt@users.noreply.github.com>
2021-11-11 12:40:38 +0700

    SulthanAbiyyu57
    Sulthan Abiyyu Tadjuddin
    151911513057

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
-import 'pages/login.dart';
+import 'pages/login.dart'; 
 
 void main() {
   runApp(MyApp());
@@ -13,20 +14,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: SplashScreen(
-        seconds: 8,
+        seconds: 30,
         navigateAfterSeconds: Login(),
         title: new Text(
           'Belajar Flutter',
           style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              color: Colors.white
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.white
           ),
         ),
-        image: Image.asset('assets/unair_mbkm.png'),
+        image:Image.asset('assets/images/1.png'),
         photoSize: 100,
         backgroundColor: Colors.blue,
         loaderColor: Colors.white,
